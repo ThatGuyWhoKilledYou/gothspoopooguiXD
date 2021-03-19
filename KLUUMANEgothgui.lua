7c85830b60f1f654ad176d4d3f3b7f2487d0142f
ThatGuyWhoKilledYou <80376083+ThatGuyWhoKilledYou@users.noreply.github.com>
2021-03-19 13:35:04 -0400

    Add files via upload

---
+++ KLUUMANEgothgui.lua
@@ -0,0 +1,396 @@
+--[[
+strings-override
+wow u can figure out opcodes, 0 fucks given XD
+haha luraph go brrrr fps go OOF
+GothX winning
+]]
+
+local GOTHX = Instance.new("ScreenGui")
+local passentryframe = Instance.new("Frame")
+local titleframe = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local cmdimg = Instance.new("ImageLabel")
+local title1 = Instance.new("TextLabel")
+local title2 = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local label2 = Instance.new("TextLabel")
+local enterkey = Instance.new("TextBox")
+local submitkey = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local label1 = Instance.new("TextLabel")
+local mainframe = Instance.new("Frame")
+local titlemain = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local maintitle = Instance.new("TextLabel")
+local maintitle_2 = Instance.new("TextLabel")
+local UIbyKLUUMANE = Instance.new("TextLabel")
+local UICorner_5 = Instance.new("UICorner")
+local attachfling_void = Instance.new("TextButton")
+local toolvoid_broken = Instance.new("TextButton")
+local spingui_psykek = Instance.new("TextButton")
+local detailframe = Instance.new("Frame")
+local voids_notice = Instance.new("TextLabel")
+local sharkyyv5 = Instance.new("TextButton")
+local shattervast = Instance.new("TextButton")
+local gothx_esp = Instance.new("TextButton")
+local lowridercar = Instance.new("TextButton")
+local essence = Instance.new("TextButton")
+local autofarm = Instance.new("TextButton")
+
+--Properties:
+
+GOTHX.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+GOTHX.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+GOTHX.ResetOnSpawn = false
+
+mainframe.Name = "mainframe"
+mainframe.Parent = GOTHX
+mainframe.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+mainframe.BorderSizePixel = 0
+mainframe.Position = UDim2.new(0.103607774, 0, 0.512618244, 0)
+mainframe.Size = UDim2.new(0, 347, 0, 230)
+mainframe.Visible = true
+
+titlemain.Name = "titlemain"
+titlemain.Parent = mainframe
+titlemain.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+titlemain.BorderSizePixel = 0
+titlemain.Position = UDim2.new(-0.000207941979, 0, -0.00253736973, 0)
+titlemain.Size = UDim2.new(0, 347, 0, 32)
+
+UICorner_4.CornerRadius = UDim.new(0, 4)
+UICorner_4.Parent = titlemain
+
+maintitle.Name = "maintitle"
+maintitle.Parent = titlemain
+maintitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+maintitle.BackgroundTransparency = 1.000
+maintitle.Position = UDim2.new(0.0144092105, 0, 0.09375, 0)
+maintitle.Size = UDim2.new(0, 55, 0, 29)
+maintitle.Font = Enum.Font.Nunito
+maintitle.Text = "Goth"
+maintitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+maintitle.TextScaled = true
+maintitle.TextSize = 14.000
+maintitle.TextWrapped = true
+
+maintitle_2.Name = "maintitle"
+maintitle_2.Parent = titlemain
+maintitle_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+maintitle_2.BackgroundTransparency = 1.000
+maintitle_2.Position = UDim2.new(0.132564843, 0, 0, 0)
+maintitle_2.Size = UDim2.new(0, 41, 0, 32)
+maintitle_2.Font = Enum.Font.Nunito
+maintitle_2.Text = "X"
+maintitle_2.TextColor3 = Color3.fromRGB(255, 0, 0)
+maintitle_2.TextScaled = true
+maintitle_2.TextSize = 14.000
+maintitle_2.TextWrapped = true
+
+UIbyKLUUMANE.Name = "UIbyKLUUMANE"
+UIbyKLUUMANE.Parent = titlemain
+UIbyKLUUMANE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+UIbyKLUUMANE.BackgroundTransparency = 1.000
+UIbyKLUUMANE.BorderSizePixel = 0
+UIbyKLUUMANE.Position = UDim2.new(0.64862287, 0, 0.143237114, 0)
+UIbyKLUUMANE.Size = UDim2.new(0, 120, 0, 22)
+UIbyKLUUMANE.Font = Enum.Font.Nunito
+UIbyKLUUMANE.Text = "UI by KLUUMANE"
+UIbyKLUUMANE.TextColor3 = Color3.fromRGB(255, 255, 255)
+UIbyKLUUMANE.TextScaled = true
+UIbyKLUUMANE.TextSize = 14.000
+UIbyKLUUMANE.TextWrapped = true
+
+UICorner_5.CornerRadius = UDim.new(0, 4)
+UICorner_5.Parent = mainframe
+
+attachfling_void.Name = "attachfling_void"
+attachfling_void.Parent = mainframe
+attachfling_void.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+attachfling_void.BorderSizePixel = 0
+attachfling_void.Position = UDim2.new(0.0144092217, 0, 0.243478253, 0)
+attachfling_void.Size = UDim2.new(0, 106, 0, 37)
+attachfling_void.Font = Enum.Font.SourceSans
+attachfling_void.Text = "Attach Void/ Fling Void"
+attachfling_void.TextColor3 = Color3.fromRGB(255, 255, 255)
+attachfling_void.TextScaled = true
+attachfling_void.TextSize = 14.000
+attachfling_void.TextWrapped = true
+
+toolvoid_broken.Name = "toolvoid_broken"
+toolvoid_broken.Parent = mainframe
+toolvoid_broken.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+toolvoid_broken.BorderSizePixel = 0
+toolvoid_broken.Position = UDim2.new(0.342939466, 0, 0.243478253, 0)
+toolvoid_broken.Size = UDim2.new(0, 106, 0, 37)
+toolvoid_broken.Font = Enum.Font.SourceSans
+toolvoid_broken.Text = "Tool Void"
+toolvoid_broken.TextColor3 = Color3.fromRGB(255, 255, 255)
+toolvoid_broken.TextScaled = true
+toolvoid_broken.TextSize = 14.000
+toolvoid_broken.TextWrapped = true
+
+spingui_psykek.Name = "spingui_psykek"
+spingui_psykek.Parent = mainframe
+spingui_psykek.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+spingui_psykek.BorderSizePixel = 0
+spingui_psykek.Position = UDim2.new(0.674351633, 0, 0.243478253, 0)
+spingui_psykek.Size = UDim2.new(0, 106, 0, 37)
+spingui_psykek.Font = Enum.Font.SourceSans
+spingui_psykek.Text = "Spin GUI (psykek)"
+spingui_psykek.TextColor3 = Color3.fromRGB(255, 255, 255)
+spingui_psykek.TextScaled = true
+spingui_psykek.TextSize = 14.000
+spingui_psykek.TextWrapped = true
+
+detailframe.Name = "detailframe"
+detailframe.Parent = mainframe
+detailframe.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+detailframe.BorderSizePixel = 0
+detailframe.Position = UDim2.new(0, 0, 0.452173918, 0)
+detailframe.Size = UDim2.new(0, 347, 0, 1)
+
+voids_notice.Name = "voids_notice"
+voids_notice.Parent = mainframe
+voids_notice.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+voids_notice.BackgroundTransparency = 1.000
+voids_notice.Position = UDim2.new(0.132564843, 0, 0.11304348, 0)
+voids_notice.Size = UDim2.new(0, 255, 0, 29)
+voids_notice.Font = Enum.Font.PermanentMarker
+voids_notice.Text = "Void/Fling (AUTHORISATION REQ.)"
+voids_notice.TextColor3 = Color3.fromRGB(255, 0, 0)
+voids_notice.TextScaled = true
+voids_notice.TextSize = 14.000
+voids_notice.TextStrokeTransparency = 0.000
+voids_notice.TextWrapped = true
+
+sharkyyv5.Name = "sharkyyv5"
+sharkyyv5.Parent = mainframe
+sharkyyv5.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+sharkyyv5.BorderSizePixel = 0
+sharkyyv5.Position = UDim2.new(0.0144092217, 0, 0.513043463, 0)
+sharkyyv5.Size = UDim2.new(0, 106, 0, 40)
+sharkyyv5.Font = Enum.Font.SourceSans
+sharkyyv5.Text = "Sharkyy V5 (vo_xo)"
+sharkyyv5.TextColor3 = Color3.fromRGB(255, 255, 255)
+sharkyyv5.TextScaled = true
+sharkyyv5.TextSize = 14.000
+sharkyyv5.TextWrapped = true
+
+shattervast.Name = "shattervast"
+shattervast.Parent = mainframe
+shattervast.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+shattervast.BorderSizePixel = 0
+shattervast.Position = UDim2.new(0.342939466, 0, 0.513043463, 0)
+shattervast.Size = UDim2.new(0, 106, 0, 40)
+shattervast.Font = Enum.Font.SourceSans
+shattervast.Text = "Shattervast (illremember)"
+shattervast.TextColor3 = Color3.fromRGB(255, 255, 255)
+shattervast.TextScaled = true
+shattervast.TextSize = 14.000
+shattervast.TextWrapped = true
+
+gothx_esp.Name = "gothx_esp"
+gothx_esp.Parent = mainframe
+gothx_esp.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+gothx_esp.BorderSizePixel = 0
+gothx_esp.Position = UDim2.new(0.674351573, 0, 0.513043463, 0)
+gothx_esp.Size = UDim2.new(0, 106, 0, 40)
+gothx_esp.Font = Enum.Font.SourceSans
+gothx_esp.Text = "GothX ESP     (KLUUMANE)"
+gothx_esp.TextColor3 = Color3.fromRGB(255, 255, 255)
+gothx_esp.TextScaled = true
+gothx_esp.TextSize = 14.000
+gothx_esp.TextWrapped = true
+
+lowridercar.Name = "lowridercar"
+lowridercar.Parent = mainframe
+lowridercar.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+lowridercar.BorderSizePixel = 0
+lowridercar.Position = UDim2.new(0.0144092217, 0, 0.769565225, 0)
+lowridercar.Size = UDim2.new(0, 106, 0, 40)
+lowridercar.Font = Enum.Font.SourceSans
+lowridercar.Text = "Slam/Stance your car"
+lowridercar.TextColor3 = Color3.fromRGB(255, 255, 255)
+lowridercar.TextScaled = true
+lowridercar.TextSize = 14.000
+lowridercar.TextWrapped = true
+
+essence.Name = "essence"
+essence.Parent = mainframe
+essence.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+essence.BorderSizePixel = 0
+essence.Position = UDim2.new(0.342939466, 0, 0.769565225, 0)
+essence.Size = UDim2.new(0, 106, 0, 40)
+essence.Font = Enum.Font.SourceSans
+essence.Text = "Essence (sirelkilla)"
+essence.TextColor3 = Color3.fromRGB(255, 255, 255)
+essence.TextScaled = true
+essence.TextSize = 14.000
+essence.TextWrapped = true
+
+autofarm.Name = "autofarm"
+autofarm.Parent = mainframe
+autofarm.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+autofarm.BorderSizePixel = 0
+autofarm.Position = UDim2.new(0.674351573, 0, 0.769565225, 0)
+autofarm.Size = UDim2.new(0, 106, 0, 40)
+autofarm.Font = Enum.Font.SourceSans
+autofarm.Text = "Autofarm GUI (unknown)"
+autofarm.TextColor3 = Color3.fromRGB(255, 255, 255)
+autofarm.TextScaled = true
+autofarm.TextSize = 14.000
+autofarm.TextWrapped = true
+
+-- Scripts:
+
+local function LDSTEUZ_fake_script() -- attachfling_void.LocalScript 
+	local script = Instance.new('LocalScript', attachfling_void)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/2xvPY1yG"))()
+	end)
+end
+coroutine.wrap(LDSTEUZ_fake_script)()
+local function CWZII_fake_script() -- toolvoid_broken.LocalScript 
+	local script = Instance.new('LocalScript', toolvoid_broken)
+
+	function leftClick()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/u31PfgLM'),true))()
+	end 
+	script.Parent.MouseButton1Click:Connect(leftClick)
+	end
+	
+coroutine.wrap(CWZII_fake_script)()
+local function KSSYGZ_fake_script() -- spingui_psykek.LocalScript 
+	local script = Instance.new('LocalScript', spingui_psykek)
+	
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/6njC2ysa"))()
+	end)
+end
+coroutine.wrap(KSSYGZ_fake_script)()
+local function PVCF_fake_script() -- sharkyyv5.LocalScript 
+	local script = Instance.new('LocalScript', sharkyyv5)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/SwissArmyTaco/V5/master/LOLFAG"))()
+	end)
+end
+coroutine.wrap(PVCF_fake_script)()
+local function RTEA_fake_script() -- shattervast.LocalScript 
+	local script = Instance.new('LocalScript', shattervast)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/AucE1f6E"))()
+	end)
+end
+coroutine.wrap(RTEA_fake_script)()
+local function USKHXME_fake_script() -- mainframe.Dragify 
+	local script = Instance.new('LocalScript', mainframe)
+
+	local UIS = game:GetService("UserInputService")
+	function dragify(Frame)
+	    dragToggle = nil
+	    local dragSpeed = 0.50
+	    dragInput = nil
+	    dragStart = nil
+	    local dragPos = nil
+	    function updateInput(input)
+	        local Delta = input.Position - dragStart
+	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
+	    end
+	    Frame.InputBegan:Connect(function(input)
+	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+	            dragToggle = true
+	            dragStart = input.Position
+	            startPos = Frame.Position
+	            input.Changed:Connect(function()
+	                if input.UserInputState == Enum.UserInputState.End then
+	                    dragToggle = false
+	                end
+	            end)
+	        end
+	    end)
+	    Frame.InputChanged:Connect(function(input)
+	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+	            dragInput = input
+	        end
+	    end)
+	    game:GetService("UserInputService").InputChanged:Connect(function(input)
+	        if input == dragInput and dragToggle then
+	            updateInput(input)
+	        end
+	    end)
+	end
+	
+	dragify(script.Parent)
+end
+coroutine.wrap(USKHXME_fake_script)()
+local function ZEZYUBF_fake_script() -- gothx_esp.LocalScript 
+	local script = Instance.new('LocalScript', gothx_esp)
+
+	
+	script.Parent.MouseButton1Down:Connect(function()
+	
+	local esp_settings = {
+		textsize = 12,
+		colour = 255,255,255
+	}
+	
+	local gui = Instance.new("BillboardGui")
+	local esp = Instance.new("TextLabel",gui)
+	
+	
+	
+	gui.Name = "GOTHX_ESP";
+	gui.ResetOnSpawn = false
+	gui.AlwaysOnTop = true;
+	gui.LightInfluence = 0;
+	gui.Size = UDim2.new(1.75, 0, 1.75, 0);
+	esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+	esp.Text = ""
+	esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+	esp.BorderSizePixel = 4;
+	esp.BorderColor3 = Color3.new(esp_settings.colour)
+	esp.BorderSizePixel = 0
+	esp.Font = "GothamSemibold"
+	esp.TextSize = esp_settings.textsize
+	esp.TextColor3 = Color3.fromRGB(esp_settings.colour)
+	
+	game:GetService("RunService").RenderStepped:Connect(function()
+		for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+			if v ~= game:GetService("Players").LocalPlayer and v.Character.Head:FindFirstChild("GOTHX_ESP") == nil then
+				esp.Text = "|"..v.Name.."|"
+				gui:Clone().Parent = v.Character.Head
+				end
+			end
+		end)
+	end)
+end
+coroutine.wrap(ZEZYUBF_fake_script)()
+local function PFWTXQB_fake_script() -- lowridercar.LocalScript 
+	local script = Instance.new('LocalScript', lowridercar)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/gP0Nxm5s"))()
+	end)
+end
+coroutine.wrap(PFWTXQB_fake_script)()
+local function ZMOKFAI_fake_script() -- essence.LocalScript 
+	local script = Instance.new('LocalScript', essence)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/vU86kLYG"))()
+	end)
+end
+coroutine.wrap(ZMOKFAI_fake_script)()
+local function LWALWBQ_fake_script() -- autofarm.LocalScript 
+	local script = Instance.new('LocalScript', autofarm)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/xFrN07ud"))()
+	end)
+end
+coroutine.wrap(LWALWBQ_fake_script)()
